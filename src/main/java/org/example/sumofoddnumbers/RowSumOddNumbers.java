493b83207d78481c2a0f96d41b0ed2c86b9c90ed
Murat Kirkoroglu <m.kirkoroglu@web.de>
2020-04-25 23:50:33 +0200

    kata sum of odd numbers successful

--- src/main/java/org/example/sumofoddnumbers/RowSumOddNumbers.java
+++ src/main/java/org/example/sumofoddnumbers/RowSumOddNumbers.java
@@ -1,8 +1,29 @@
 package org.example.sumofoddnumbers;
 
+import java.util.ArrayList;
+
 public class RowSumOddNumbers {
     public static int rowSumOddNumbers(int n) {
-        //TODO
-        return 0;
+        int number = 0;
+        int value = 0;
+        int result = 0;
+        ArrayList<Integer> listArray = new ArrayList<Integer>();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                number++;
+                if (number % 2 == 1) {
+                    listArray.add(number);
+                    for (int val : listArray) {
+                        value += val;
+                    }
+                    result = value;
+                    number++;
+                }
+                listArray.clear();
+            }
+            value = 0;
+        }
+        return result;
     }
 }
--- src/test/java/org/example/sumofoddnumbers/RowSumOddNumbersTest.java
+++ src/test/java/org/example/sumofoddnumbers/RowSumOddNumbersTest.java
@@ -1,13 +1,27 @@
 package org.example.sumofoddnumbers;
 
+import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class RowSumOddNumbersTest {
+
+
+    private RowSumOddNumbers rowSumOddNumbers;
+
+    @Before
+    public void setup() {
+        rowSumOddNumbers = new RowSumOddNumbers();
+    }
+
     @Test
     public void test1() {
         assertEquals(1, RowSumOddNumbers.rowSumOddNumbers(1));
+    }
+
+    @Test
+    public void test2() {
         assertEquals(74088, RowSumOddNumbers.rowSumOddNumbers(42));
     }
 }
